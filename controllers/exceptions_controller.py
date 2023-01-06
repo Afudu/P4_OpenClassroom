98f2920ba7ea79654a9958ea4f69f2bddc4989f8
Afudu <abdoulseydou@hotmail.com>
2023-01-06 01:15:14 +0000

    Create exceptions controller

---
+++ controllers/exceptions_controller.py
@@ -0,0 +1,13 @@
+class MatchLostError(Exception):
+    """Exception raised when the match is lost and the score input is not 0"""
+    pass
+
+
+class MatchWonError(Exception):
+    """Exception raised when the match is won and the score input is not 1"""
+    pass
+
+
+class MatchTieError(Exception):
+    """Exception raised when the match is a tie and the score input is not 0.5"""
+    pass
