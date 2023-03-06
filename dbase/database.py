1ff2feb0002db0b1cda71d0937a93498c9a93b1f
Afudu <abdoulseydou@hotmail.com>
2023-03-06 00:33:46 +0000

    Update dbase

--- dbase/database.py
+++ dbase/database.py
@@ -1,9 +1,9 @@
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
 
 
 class Database:
     db = TinyDB('dbase/db.json', indent=4)
-    Tournament_Query = Query()
+    # Tournament_Query = Query()
     tournaments_table = db.table('tournaments_table')
     players_table = db.table('players_table')
     rounds_table = db.table('rounds_table')
